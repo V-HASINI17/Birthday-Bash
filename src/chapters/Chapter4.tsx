38847d47dfa950d652e181a704cce9776cce22c6
Hasini <vegesnahasini@gmail.com>
2026-09-11 18:31:39 +0530

    Center and style 'Next' button

--- src/chapters/Chapter4.tsx
+++ src/chapters/Chapter4.tsx
@@ -95,6 +95,7 @@ function ChildhoodPhotoScene({ onNext }: { onNext: () => void }) {
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ delay: 0.5 }}
+            className="flex justify-center mt-4"
           >
             <NextButton onClick={onNext} />
           </motion.div>
